5c97fe6a9e88d855ad62c665d80028e8cd175889
emilyfranceskolar@gmail.com <emilyfranceskolar@gmail.com>
2026-02-20 16:03:55 +0100

    removed obsticle.ts again

--- src/obsticle.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="entity.ts" />
-class Obstacle extends Entity {
-  constructor(worldX: number, worldY: number, ) {
-    super(worldX, worldY, images.tiles.floor);
-  }
-
-  public update() {}
-  public onCollision() {}
-}
