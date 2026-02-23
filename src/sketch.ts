08ca2be54a623bcd9d3db6747b972477ed67bba6
Josefine Carlson <josefinecarlson@MacBook-Air-som-tillhor-Josefine-2.local>
2026-02-23 15:12:40 +0100

    fixed dots in level1

--- src/sketch.ts
+++ src/sketch.ts
@@ -66,8 +66,8 @@ function preload() {
   };
 
   levels = [
-    // loadStrings("assets/levels/level1.txt"), // Level 1
-    loadStrings('assets/levels/level2.txt'), // Level 2
+    loadStrings("assets/levels/level1.txt"), // Level 1
+    // loadStrings('assets/levels/level2.txt'), // Level 2
   ];
 }
 
