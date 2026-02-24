38bdb2b4713f6cd96f85c94c5bc0aee42bdcabcb
txmpold <love.tampold@hotmail.com>
2026-02-24 14:42:44 +0100

    added code to startmenu

--- src/startMenu.ts
+++ src/startMenu.ts
@@ -3,16 +3,19 @@ class StartMenu {
   private startButton: Button;
   private showControls: Button;
 
-  constructor(game: new Game) {
-    this.game = new Game();
-    this.startButton = new Button("Start Game", onclick,  );
-    this.showControls = new Button("Show Controls", ;
+  constructor(game: Game) {
+    this.game = game;
+    this.startButton = new Button("Start Game", () => this.startGame(), createVector(150, 50), createVector(100, 100), createVector(100, 100));
+    this.showControls = new Button("Show Controls", () => this.drawControls(), createVector(150, 50), createVector(100, 200), createVector(100, 200));
   }
 
 
 
   public update() {}
-  public draw() {}
+  public draw() {
+    this.startButton.draw();
+    this.showControls.draw();
+  }
   public drawControls() {}
   public startGame() {}
 }
