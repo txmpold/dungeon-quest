4828481d0b8a6f7992ab5d6c2f09e760d9d1812f
txmpold <love.tampold@hotmail.com>
2026-02-24 14:42:29 +0100

    comment code to test menu

--- src/game.ts
+++ src/game.ts
@@ -4,18 +4,19 @@ class Game {
   private startMenu: StartMenu;
 
   constructor() {
-    this.levelFactory = new LevelFactory(tiles, 0, 0);
-    this.level = this.levelFactory.generateLevel(0);
+    // this.levelFactory = new LevelFactory(tiles, 0, 0);
+    // this.level = this.levelFactory.generateLevel(0);
     this.startMenu = new StartMenu(this);
   }
 
   public update() {
-    this.level.update();
+    // this.level.update();
   }
 
   public draw() {
     background("white");
-    this.level.draw();
+    this.startMenu.draw();
+    // this.level.draw();
     // this.levelFactory.draw();
   }
 }
