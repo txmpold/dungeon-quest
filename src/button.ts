eed7075b1af890262c77a0131477de0de116efa8
txmpold <love.tampold@hotmail.com>
2026-02-24 13:09:56 +0100

    button is drawn (wip)

--- src/button.ts
+++ src/button.ts
@@ -2,14 +2,23 @@ class Button {
 private label: string;
 private onClick: () => void;
 private size: p5.Vector
+private buttonPos: p5.Vector
+private buttonTextPos: p5.Vector
 
-    constructor(label: string, onClick: () => void, size: creaeVector()) {
+    constructor(label: string, onClick: () => void, size: p5.Vector, buttonPos: p5.Vector, buttonTextPos: p5.Vector) {
     this.label = label;
     this.onClick = onClick;
-    this.size = size;
+    this.size = createVector(size.x, size.y);
+    this.buttonPos = createVector(buttonPos.x, buttonPos.y);
+    this.buttonTextPos = createVector(buttonTextPos.x, buttonTextPos.y);
     }
 
+    draw() {
+        text(this.label, this.buttonTextPos.x + 10, this.buttonTextPos.y + 20);
+        textSize(16);
+        textAlign(LEFT, TOP);
+        rect(this.buttonPos.x, this.buttonPos.y, this.size.x, this.size.y);
+        fill("orange");
+    }
     
 }
-
-
