f8f804a75ac2a2f7a62179aaaeb5a3ada5f02dde
joscarlson90 <josefinecarlson90@gmail.com>
2026-02-26 14:18:32 +0100

    added more props, walls, floor

--- src/levelFactory.ts
+++ src/levelFactory.ts
@@ -25,78 +25,141 @@ class LevelFactory {
 
       const entityNumber = levelGrid[worldRow][worldCol];
       if (
-        entityNumber === '.' ||
-        entityNumber === 'g' ||
-        entityNumber === 'b' ||
-        entityNumber === 't' ||
-        entityNumber === 'X' ||
-        entityNumber === 'Y' ||
-        entityNumber === 'Z' ||
-        entityNumber === 'V' 
+        entityNumber === "." ||
+        entityNumber === "g" ||
+        entityNumber === "b" ||
+        entityNumber === "t" ||
+        entityNumber === "c" ||
+        entityNumber === "d" ||
+        entityNumber === "e" ||
+        entityNumber === "f" ||
+        entityNumber === "X" ||
+        entityNumber === "E" ||
+        entityNumber === "H" ||
+        entityNumber === "I" ||
+        entityNumber === "J" ||
+        entityNumber === "K" ||
+        entityNumber === "L" ||
+        entityNumber === "M" ||
+        entityNumber === "N" ||
+        entityNumber === "Y" ||
+        entityNumber === "Z" ||
+        entityNumber === "V"
       ) {
         entities.push(new Floor(worldX, worldY));
       }
-      if (entityNumber === '2') {
+      if (entityNumber === "2") {
         entities.push(new Obstacle(worldX, worldY, images.tiles.wall));
       }
-      if (entityNumber === 'W') {
+      if (entityNumber === "W") {
         entities.push(new Obstacle(worldX, worldY, images.tiles.water));
       }
-      if (entityNumber === '5') {
+      if (entityNumber === "5") {
         entities.push(new Obstacle(worldX, worldY, images.tiles.wallTop));
       }
-      if (entityNumber === '4') {
+      if (entityNumber === "4") {
         entities.push(new Floor(worldX, worldY));
         player = new Player(worldX, worldY, 5, level);
       }
-      if (entityNumber === '6') {
+      if (entityNumber === "6") {
         entities.push(new Obstacle(worldX, worldY, images.tiles.wallDown));
       }
-      if (entityNumber === '7') {
+      if (entityNumber === "7") {
         entities.push(new Obstacle(worldX, worldY, images.tiles.wallLeft));
       }
-      if (entityNumber === '8') {
+      if (entityNumber === "8") {
         entities.push(new Obstacle(worldX, worldY, images.tiles.wallRight));
       }
-      if (entityNumber === 'A') {
+      if (entityNumber === "A") {
         entities.push(
           new Obstacle(worldX, worldY, images.tiles.wallTopLeftCorner),
         );
       }
-      if (entityNumber === 'B') {
+      if (entityNumber === "B") {
         entities.push(
           new Obstacle(worldX, worldY, images.tiles.wallTopRightCorner),
         );
       }
-      if (entityNumber === 'C') {
+      if (entityNumber === "C") {
         entities.push(
           new Obstacle(worldX, worldY, images.tiles.wallDownLeftCorner),
         );
       }
-      if (entityNumber === 'D') {
+      if (entityNumber === "D") {
         entities.push(
           new Obstacle(worldX, worldY, images.tiles.wallDownRightCorner),
         );
       }
-      if (entityNumber === 'g') {
+      if (entityNumber === "F") {
+        entities.push(new Obstacle(worldX, worldY, images.tiles.wallShadow));
+      }
+      if (entityNumber === "H") {
+        entities.push(new Obstacle(worldX, worldY, images.tiles.wallBottom));
+      }
+      if (entityNumber === "E") {
+        entities.push(
+          new Obstacle(worldX, worldY, images.tiles.wallCornerSpec),
+        );
+      }
+      if (entityNumber === "I") {
+        entities.push(
+          new Obstacle(worldX, worldY, images.tiles.wallCornerSpec2),
+        );
+      }
+      if (entityNumber === "J") {
+        entities.push(
+          new Obstacle(worldX, worldY, images.tiles.wallCornerSpec3),
+        );
+      }
+      if (entityNumber === "K") {
+        entities.push(
+          new Obstacle(worldX, worldY, images.tiles.wallCornerSpec4),
+        );
+      }
+      if (entityNumber === "L") {
+        entities.push(new Obstacle(worldX, worldY, images.tiles.floorCircle));
+      }
+      if (entityNumber === "M") {
+        entities.push(
+          new Obstacle(worldX, worldY, images.tiles.wallCornerSpec5),
+        );
+      }
+       if (entityNumber === "N") {
+        entities.push(
+          new Obstacle(worldX, worldY, images.tiles.floorStone),
+        );
+      }
+      if (entityNumber === "g") {
         entities.push(new Obstacle(worldX, worldY, images.tiles.ghost));
       }
-      if (entityNumber === 'b') {
+      if (entityNumber === "b") {
         entities.push(new Obstacle(worldX, worldY, images.tiles.barrel));
       }
-      if (entityNumber === 't') {
+      if (entityNumber === "t") {
         entities.push(new Obstacle(worldX, worldY, images.tiles.treasure));
       }
-      if (entityNumber === 'X') {
+      if (entityNumber === "c") {
+        entities.push(new Obstacle(worldX, worldY, images.tiles.candelabra));
+      }
+      if (entityNumber === "d") {
+        entities.push(new Obstacle(worldX, worldY, images.tiles.box1));
+      }
+      if (entityNumber === "e") {
+        entities.push(new Obstacle(worldX, worldY, images.tiles.box2));
+      }
+      if (entityNumber === "f") {
+        entities.push(new Obstacle(worldX, worldY, images.tiles.box3));
+      }
+      if (entityNumber === "X") {
         entities.push(new GreenEnemy(worldX, worldY, 3));
       }
-      if (entityNumber === 'Y') {
+      if (entityNumber === "Y") {
         entities.push(new BlueEnemy(worldX, worldY, 4));
       }
-      if (entityNumber === 'Z') {
+      if (entityNumber === "Z") {
         entities.push(new RedEnemy(worldX, worldY, 5));
       }
-      if (entityNumber === 'V') {
+      if (entityNumber === "V") {
         entities.push(new Boss(worldX, worldY, 10));
       }
 
@@ -109,7 +172,7 @@ class LevelFactory {
     }
 
     if (!player) {
-      throw new Error('Missing player in data');
+      throw new Error("Missing player in data");
     }
     entities.push(player);
 
--- src/sketch.ts
+++ src/sketch.ts
@@ -32,6 +32,15 @@ let images: {
     box1: p5.Image;
     box2: p5.Image;
     box3: p5.Image;
+    wallShadow: p5.Image;
+    floorStone: p5.Image;
+    wallBottom: p5.Image;
+    wallCornerSpec: p5.Image;
+    wallCornerSpec2: p5.Image;
+    wallCornerSpec3: p5.Image;
+    wallCornerSpec4: p5.Image;
+    wallCornerSpec5: p5.Image;
+    floorCircle: p5.Image;
   };
 };
 
@@ -45,39 +54,52 @@ let levels: string[][];
  */
 function preload() {
   music = {
-    mystery: loadSound('assets/music/mystery.mp3'),
+    mystery: loadSound("assets/music/mystery.mp3"),
   };
   soundEffects = {
-    shoot: loadSound('assets/soundEffects/laserShoot.mp3'),
+    shoot: loadSound("assets/soundEffects/laserShoot.mp3"),
   };
   images = {
     // tile: loadImage("assets/images/tile.png"),
-    map: loadImage('assets/images/josefineLevelimg.png'),
-    character: loadImage('assets/images/character.png'),
-    slime: loadImage('assets/images/slims.png'),
-    boss: loadImage('assets/images/boss_.png'),
-    weapon: loadImage('assets/images/fireball.png'),
+    map: loadImage("assets/images/josefineLevelimg.png"),
+    character: loadImage("assets/images/character.png"),
+    slime: loadImage("assets/images/slims.png"),
+    boss: loadImage("assets/images/boss_.png"),
+    weapon: loadImage("assets/images/fireball.png"),
     tiles: {
-      floor: loadImage('assets/tiles/floor.png'),
-      wall: loadImage('assets/tiles/wall.png'),
-      water: loadImage('assets/tiles/water.png'),
-      wallTop: loadImage('assets/tiles/wall-top.png'),
-      wallDown: loadImage('assets/tiles/wall-down.png'),
-      wallLeft: loadImage('assets/tiles/wall-left.png'),
-      wallRight: loadImage('assets/tiles/wall-right.png'),
-      wallTopLeftCorner: loadImage('assets/tiles/wall-top-left-corner.png'),
-      wallTopRightCorner: loadImage('assets/tiles/wall-top-right-corner.png'),
-      wallDownLeftCorner: loadImage('assets/tiles/wall-down-left-corner.png'),
-      wallDownRightCorner: loadImage('assets/tiles/wall-down-right-corner.png'),
-      ghost: loadImage('assets/tiles/ghost.png'),
-      barrel: loadImage('assets/tiles/barrel.png'),
-      treasure: loadImage('assets/tiles/treasure.png'),
+      floor: loadImage("assets/tiles/floor.png"),
+      wall: loadImage("assets/tiles/wall.png"),
+      water: loadImage("assets/tiles/water.png"),
+      wallTop: loadImage("assets/tiles/wall-top.png"),
+      wallDown: loadImage("assets/tiles/wall-down.png"),
+      wallLeft: loadImage("assets/tiles/wall-left.png"),
+      wallRight: loadImage("assets/tiles/wall-right.png"),
+      wallTopLeftCorner: loadImage("assets/tiles/wall-top-left-corner.png"),
+      wallTopRightCorner: loadImage("assets/tiles/wall-top-right-corner.png"),
+      wallDownLeftCorner: loadImage("assets/tiles/wall-down-left-corner.png"),
+      wallDownRightCorner: loadImage("assets/tiles/wall-down-right-corner.png"),
+      ghost: loadImage("assets/tiles/ghost.png"),
+      barrel: loadImage("assets/tiles/barrel.png"),
+      treasure: loadImage("assets/tiles/treasure.png"),
+      candelabra: loadImage("assets/tiles/candelabra.png"),
+      box1: loadImage("assets/tiles/box1.png"),
+      box2: loadImage("assets/tiles/box2.png"),
+      box3: loadImage("assets/tiles/box3.png"),
+      wallShadow: loadImage("assets/tiles/wall-shadow.png"),
+      floorStone: loadImage("assets/tiles/floor-stone.png"),
+      wallBottom: loadImage("assets/tiles/wall-bottom.png"),
+      wallCornerSpec: loadImage("assets/tiles/wall-corner-spec.png"),
+      wallCornerSpec2: loadImage("assets/tiles/wall-corner-spec2.png"),
+      wallCornerSpec3: loadImage("assets/tiles/wall-corner-spec3.png"),
+      wallCornerSpec4: loadImage("assets/tiles/wall-corner-spec4.png"),
+      wallCornerSpec5: loadImage("assets/tiles/wall-corner-spec5.png"),
+      floorCircle: loadImage("assets/tiles/floor-circle.png"),
     },
   };
 
   levels = [
-    loadStrings('assets/levels/level1.txt'), // Level 1
-    // loadStrings('assets/levels/level2.txt'), // Level 2
+    loadStrings("assets/levels/level2.txt"), // Level 2
+    loadStrings("assets/levels/level1.txt"), // Level 1
   ];
 }
 
