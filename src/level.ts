f474a11ab94d0ad59c3e361c1428eb9ec26897c1
Grebbes <Grebbes@gmail.com>
2026-03-02 18:16:13 +0100

    game now only draws what is inside the player view

--- src/level.ts
+++ src/level.ts
@@ -31,17 +31,35 @@ class Level implements ILevelContext {
     background(0);
     /* image(images.map, 0, 0); */
 
+    const playerEntity = this.entities.find(
+      (e) => e instanceof Player,
+    ) as Player;
+    if (playerEntity) {
+      translate(
+        -playerEntity.worldX +
+          GamePanel.screenWidth / 2 -
+          GamePanel.tileSize / 2,
+        -playerEntity.worldY +
+          GamePanel.worldHeight / 2 -
+          GamePanel.tileSize / 2,
+      );
+    }
+
     for (let entity of this.entities) {
       if (entity instanceof Player) {
-        translate(
-          -entity.worldX + GamePanel.screenWidth / 2 - GamePanel.tileSize / 2,
-          -entity.worldY + GamePanel.worldHeight / 2 - GamePanel.tileSize / 2,
-        );
+        entity.draw();
+      } else {
+        const distX = Math.abs(entity.worldX - playerEntity.worldX);
+        const distY = Math.abs(entity.worldY - playerEntity.worldY);
+        if (
+          distX < GamePanel.screenWidth / 2 + GamePanel.tileSize &&
+          distY < GamePanel.screenHeight / 2 + GamePanel.tileSize
+        ) {
+          entity.draw();
+        }
       }
     }
-    for (let entity of this.entities) {
-      entity.draw();
-    }
+
     pop();
   }
   public drawTreasure() {}
