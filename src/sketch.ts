8307d2f342cd622dc9111c806265a02781a47b33
joscarlson90 <josefinecarlson90@gmail.com>
2026-03-02 21:46:06 +0100

    created level three and added more animated props

--- src/sketch.ts
+++ src/sketch.ts
@@ -147,8 +147,8 @@ function preload() {
 
   levels = [
     // loadStrings("assets/levels/level1.txt"), // Level 1
-    // loadStrings("assets/levels/level2.txt"), // Level 2
-    loadStrings("assets/levels/level3.txt"),
+    loadStrings("assets/levels/level2.txt"), // Level 2
+    // loadStrings("assets/levels/level3.txt"),
   ];
 
   /* isCollision = [loadImage("assets/tiles/water.png")]; kanske behöver */
