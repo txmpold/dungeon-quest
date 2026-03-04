49e576401bbc1736a8dde2053b73cb90d1b37ea6
txmpold <love.tampold@hotmail.com>
2026-03-04 14:37:25 +0100

    changed attackcooldown to 3000

--- src/player.ts
+++ src/player.ts
@@ -153,7 +153,7 @@ class Player extends Entity {
     let y = this.direction.y;
     if (keyIsDown(32) && this.attackCooldown <= 0) {
       soundEffects.fireball.play(0, 1, 5);
-      this.attackCooldown = 0;
+      this.attackCooldown = 3000;
 
       let fireballImage = images.weapons.fireball_right;
       if (y < 0) {
