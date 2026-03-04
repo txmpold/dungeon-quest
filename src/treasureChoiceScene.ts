4e6b9e96639665f12bdc6dc7339df0eddfe9eed5
txmpold <love.tampold@hotmail.com>
2026-03-04 12:19:20 +0100

    added buttons with images to treasurechoice

--- src/treasureChoiceScene.ts
+++ src/treasureChoiceScene.ts
@@ -2,12 +2,41 @@ class TreasureChoiceScene extends Scene {
   private choice1: Button;
   private choice2: Button;
 
-  constructor() {
+  constructor(choice1Imgage: p5.Image, choice2Image: p5.Image) {
     super("treasureChoiceScene");
     this.choice1 = new Button(
-      " ", push(fireball?), createVector(200, 200), createVector(GamePanel.worldWidth / 2 - 200, GamePanel.worldHeight / 2 - 200));
+      null,
+      () => {
+        music.gameMusic.play();
+        game.changeScene(
+          new TransitionScene("Level 2", 2000, () => game.goToNextLevel()),
+        );
+      },
+      createVector(200, 200),
+      createVector(
+        GamePanel.worldWidth / 2 - 250,
+        GamePanel.worldHeight / 2 - 100,
+      ),
+      choice1Imgage,
+    );
     this.choice2 = new Button(
-      " ", push(heart?), createVector(200, 200), createVector(GamePanel.worldWidth / 2 + 200, GamePanel.worldHeight / 2 - 200));
+      null,
+      () => {
+        music.gameMusic.play();
+        game.changeScene(
+          new TransitionScene("Level 2", 2000, () => game.goToNextLevel()),
+        );
+      },
+      createVector(200, 200),
+      createVector(
+        GamePanel.worldWidth / 2 + 50,
+        GamePanel.worldHeight / 2 - 100,
+      ),
+      choice2Image,
+    );
+    music.gameMusic.stop();
+    cursor("assets/images/swordcursor.png");
+    game.gameIsStarted = false;
   }
 
   public update() {
@@ -20,3 +49,4 @@ class TreasureChoiceScene extends Scene {
     this.choice1.draw();
     this.choice2.draw();
   }
+}
