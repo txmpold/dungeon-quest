51e63fea0b4054df97a77bf5315bd79a850a872f
txmpold <love.tampold@hotmail.com>
2026-03-04 20:10:37 +0100

    added player health as array in game.ts

--- src/game.ts
+++ src/game.ts
@@ -2,6 +2,7 @@ class Game {
   public levelFactory: LevelFactory;
   public level: Level;
   public playerinventory: p5.Image[];
+  public playerHealth: string[];
   private currentScene: Scene;
   public gameIsStarted: boolean = false;
 
@@ -9,6 +10,7 @@ class Game {
     this.levelFactory = new LevelFactory(tiles, 0, 0);
     this.level = new Level([]);
     this.playerinventory = [];
+    this.playerHealth = ["hp", "hp", "hp", "hp", "hp"];
     this.currentScene = new StartMenu();
   }
 
