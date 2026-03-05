7ef96bb4e663356e3598da5d7d05a2229b9aef6d
Grebbes <Grebbes@gmail.com>
2026-03-05 09:02:32 +0100

    added more enemies

--- src/blueEnemy.ts
+++ src/blueEnemy.ts
@@ -4,15 +4,19 @@ class BlueEnemy extends Enemy {
   private patrolTimer = 0;
   private nrOfPatrolSteps = 120; //frames per håll
   private shootCooldown = 0;
-   private directions = [
-    { x:0, y: -1, img: images.weapons.fireball_up }, //up
-    { x:0, y: 1, img: images.weapons.fireball_down }, //down
-    { x:-1, y:0, img: images.weapons.fireball_left }, //left 
-    { x:1, y: 0, img: images.weapons.fireball_right }, //right
+  private directions = [
+    { x: 0, y: -1, img: images.weapons.fireball_up }, //up
+    { x: 0, y: 1, img: images.weapons.fireball_down }, //down
+    { x: -1, y: 0, img: images.weapons.fireball_left }, //left
+    { x: 1, y: 0, img: images.weapons.fireball_right }, //right
   ];
 
-
-  constructor(worldX: number, worldY: number, health: number, levelContext:ILevelContext) {
+  constructor(
+    worldX: number,
+    worldY: number,
+    health: number,
+    levelContext: ILevelContext,
+  ) {
     const row = 1;
     const col = 0;
     const totalCol = 8;
@@ -33,74 +37,76 @@ class BlueEnemy extends Enemy {
     this.engage();
     super.update(entities);
   }
-  
-  //den blå monstret ska kunna gå ett steg och skjuta åt alla håll, har ett fast rörelsemönster 
-  public move(){
-      const walkSpeed = 0.08;
-      const dir = this.directions[this.patrolDirectionIndex]; 
-      let speed = createVector(dir.x, dir.y).mult(walkSpeed);
-      this.speed.set(speed);
-      this.patrolTimer++;
-      
-      if (this.patrolTimer >= this.nrOfPatrolSteps){
-        this.patrolTimer= 0; 
-        this.patrolDirectionIndex = floor(random() * this.directions.length);
-      }
-  } 
-  
+
+  //den blå monstret ska kunna gå ett steg och skjuta åt alla håll, har ett fast rörelsemönster
+  public move() {
+    const walkSpeed = 0.08;
+    const dir = this.directions[this.patrolDirectionIndex];
+    let speed = createVector(dir.x, dir.y).mult(walkSpeed);
+    this.speed.set(speed);
+    this.patrolTimer++;
+
+    if (this.patrolTimer >= this.nrOfPatrolSteps) {
+      this.patrolTimer = 0;
+      this.patrolDirectionIndex = floor(random() * this.directions.length);
+    }
+  }
+
   protected engage() {
     this.move();
-    if (this.playerIsNearby()){
-      if (this.shootCooldown > 0){
+    if (this.playerIsNearby()) {
+      if (this.shootCooldown > 0) {
         this.shootCooldown -= deltaTime;
-        if (this.shootCooldown < 200) { //labba med detta
-          this.speed.set(0,0); // stanna
+        if (this.shootCooldown < 200) {
+          //labba med detta
+          this.speed.set(0, 0); // stanna
         }
         return;
       }
       this.shootProjectile();
-      this.shootCooldown = random(1_500, 2_500);  //labba med detta
+      this.shootCooldown = random(2_500, 3_500); //labba med detta
     }
   }
   private playerIsNearby() {
-  if (!this.levelContext.player) return false;
-  let distX = abs(this.levelContext.player.worldX - this.worldX);
-  let distY = abs(this.levelContext.player.worldY - this.worldY);
-  let shootLimit = 0.4;  //procentuellt av skärmens yta
-  return distX < GamePanel.worldWidth * shootLimit && distY < GamePanel.worldHeight * shootLimit;
+    if (!this.levelContext.player) return false;
+    let distX = abs(this.levelContext.player.worldX - this.worldX);
+    let distY = abs(this.levelContext.player.worldY - this.worldY);
+    let shootLimit = 0.4; //procentuellt av skärmens yta
+    return (
+      distX < GamePanel.worldWidth * shootLimit &&
+      distY < GamePanel.worldHeight * shootLimit
+    );
   }
 
   private shootProjectile() {
-    //Om spelaren är nära stanna, skjut åt all håll 
+    //Om spelaren är nära stanna, skjut åt all håll
     if (!this.levelContext.player) return;
     const shootSpeed = 0.2;
     const offset = 40;
-    
-    for (let dir of this.directions){
+
+    for (let dir of this.directions) {
       let speed = createVector(dir.x, dir.y).mult(shootSpeed);
 
-      let shootX = this.worldX + (dir.x * offset)
-      let shootY = this.worldY + (dir.y * offset)
-  
+      let shootX = this.worldX + dir.x * offset;
+      let shootY = this.worldY + dir.y * offset;
+
       let fireball = new Projectile(
         shootX,
         shootY,
         speed,
         dir.img,
         1,
-        2,  
+        2,
         5,
         false,
       );
       this.levelContext.entities.push(fireball);
       fireball.setHitBox(
-       GamePanel.tileSize * 0.27,
-       GamePanel.tileSize * 0.4,
-       GamePanel.tileSize * 0.5,
-       GamePanel.tileSize * 0.7,
-     );
+        GamePanel.tileSize * 0.27,
+        GamePanel.tileSize * 0.4,
+        GamePanel.tileSize * 0.5,
+        GamePanel.tileSize * 0.7,
+      );
     }
-
-  }   
+  }
 }
-
